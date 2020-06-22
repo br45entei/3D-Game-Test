f74e89cd46dc04d84eca11c0ca05d748345e98ec
Brian_Entei <br45entei@gmail.com>
2020-06-21 21:59:34 -0500

    Modified code for testing on my Lenovo T420's proper gpu drivers (Win7)
    * Modified code for testing on my Lenovo T420's 'proper' gpu drivers
    with Windows 7. It doesn't seem to support anything higher than OpenGL
    3.0, and the context-creation code(lwjgl-swt) requires at least 3.2 when
    specifying the major and minor version, so I had to use zero for
    "unspecified".
    Things are still a bit buggy, but it's an outdated laptop, so what can
    you do? (e.g. setting vsync on one window but turning it off on the
    other doesn't work properly; they both share the same vsync setting [are
    they technically using the same OpenGL context?])

--- src/com/gmail/br45entei/test/MultiWindowedTest.java
+++ src/com/gmail/br45entei/test/MultiWindowedTest.java
@@ -4,13 +4,20 @@ import com.gmail.br45entei.game.graphics.Renderer;
 import com.gmail.br45entei.game.input.InputCallback.InputLogger;
 import com.gmail.br45entei.game.ui.Window;
 
+import org.lwjgl.opengl.swt.GLData;
+
 /** @author Brian_Entei */
 public class MultiWindowedTest {
 	
 	public static final void main(String[] args) {
 		Thread.currentThread().setName("FirstWindowThread");
 		
-		Window window = new Window("SWT-LWJGL3 Framework #1", 800, 600);
+		final GLData data = Window.createDefaultGLData();
+		data.forwardCompatible = false;
+		data.majorVersion = 0;
+		data.minorVersion = 0;
+		
+		Window window = new Window("SWT-LWJGL3 Framework #1", 800, 600, data);
 		window.registerInputCallback(new InputLogger(System.out));
 		window.setActiveRenderer(new Renderer.ColorDemo() {
 			@Override
@@ -19,7 +26,7 @@ public class MultiWindowedTest {
 				Thread thread = new Thread(new Runnable() {
 					@Override
 					public void run() {
-						Window window = new Window("SWT-LWJGL3 Framework #2", 800, 600);
+						Window window = new Window("SWT-LWJGL3 Framework #2", 800, 600, data);
 						window.registerInputCallback(new InputLogger(System.err));
 						window.setActiveRenderer(new Renderer.ColorDemo());
 						window.open();
